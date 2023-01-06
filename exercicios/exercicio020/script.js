86de2c1ba98ecf90f2449b5f8a7fac4df503a07b
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-01-06 17:46:05 -0300

    update ex020

--- exercicios/exercicio020/script.js
+++ exercicios/exercicio020/script.js
@@ -1,3 +1,8 @@
-let num = [5, 8, 9, 3]
-
-console.log(`Nosso vetor é o ${num}`)
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`o vetor tem ${num.length} posições`)
+console.log(`o Primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
